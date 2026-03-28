aa5a38aa259c3a32c7b6cad5fc22bad62766a434
ShritanGopu <gopushritan@gmail.com>
2026-03-28 16:52:48 -0700

    mushr_rhc properly running and built. Lagging on my computer worked for a little bit and then it didn't lol. Need to try on the jetson and ethans

--- mushr_hardware/ydlidar_ros2_driver/startup/initenv.sh
+++ mushr_hardware/ydlidar_ros2_driver/startup/initenv.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar.rules
+set -e
 
-echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar-V2.rules
-
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar-2303.rules
-
-service udev reload
-sleep 2
-service udev restart
-
+# This lidar is addressed through the kernel-managed /dev/serial/by-id path,
+# so no custom udev alias is required here.
+echo "No custom YDLIDAR udev rule required. Use /dev/serial/by-id/... in the driver params."
